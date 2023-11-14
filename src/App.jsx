6e0906495b81132d75804c4c98381b353f070462
Renier Vargas <renier1989@gmail.com>
2023-11-13 21:06:03 -0500

    347. Primeros pasos con el Proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-
-
+import { Container } from "react-bootstrap";
 function App() {
-  
-
   return (
     <>
-    <h1>Drink Finder</h1>
+      <header className="py-5">
+        <h1>Drink Finder</h1>
+      </header>
+
+      <Container className="mt-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, blanditiis ea.
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
