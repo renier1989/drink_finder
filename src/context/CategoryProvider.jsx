c361f883264646f5fd549d6cdf0cb403730c68ce
renier1989 <renier1989@gmail.com>
2023-11-14 17:11:05 -0500

    351. Iterando sobre las Categorias

--- src/context/CategoryProvider.jsx
+++ src/context/CategoryProvider.jsx
@@ -1,10 +1,29 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useEffect, useState } from "react";
 
 const CategoryContext = createContext();
 
 const CategoryProvider = ({children}) => {
+    const [categories , setCategories] = useState([]);
+    const getCategories = async () =>{
+        try {
+            
+            const url = `https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`
+            const {data} = await axios(url);
+            setCategories(data.drinks);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+        getCategories()
+    },[]);
+
     return (
-        <CategoryContext.Provider value={{}}>
+        <CategoryContext.Provider value={{
+            categories
+        }}>
             {children}
         </CategoryContext.Provider>
 
