039fd1e63422bb5a2b17277fdfc20cf28e44ef2e
renier1989 <renier1989@gmail.com>
2023-11-15 16:37:59 -0500

    359. Finalizando el Modal con todos los datos de la receta

--- src/components/DrinkModal.jsx
+++ src/components/DrinkModal.jsx
@@ -1,14 +1,35 @@
-import { Modal } from "react-bootstrap"
-import useDrinks from "../hooks/useDrinks"
-
+import { Image, Modal } from "react-bootstrap";
+import useDrinks from "../hooks/useDrinks";
 
 export const DrinkModal = () => {
-    const {modal, handleShowModal, recipe} = useDrinks()
+  const { modal, handleShowModal, recipe } = useDrinks();
+  console.log(recipe);
+  const showIngredients = () => {
+    let ingredients = [];
+
+    for (let i = 1; i < 16; i++) {
+      if (recipe[`strIngredient${i}`]) {
+        ingredients.push(
+          <li>
+            {recipe[`strIngredient${i}`]} {recipe[`strMeasure${i}`]}
+          </li>
+        );
+      }
+    }
+    return ingredients;
+  };
   return (
     <Modal show={modal} onHide={handleShowModal}>
-        <Modal.Body>
-            Modal Body here
-        </Modal.Body>
+      <Image src={recipe.strDrinkThumb} alt={`image ${recipe.strDrink}`} />
+      <Modal.Title className="mx-3">
+        <h2>{recipe.strDrink}</h2>
+      </Modal.Title>
+      <Modal.Body>
+        <h4>Instructions</h4>
+        {recipe.strInstructions}
+        <h4>Ingredients</h4>
+        {showIngredients()}
+      </Modal.Body>
     </Modal>
-  )
-}
+  );
+};
