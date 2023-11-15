eeec60de90e6061a050899e330d7ee675b2e5274
Renier Vargas <renier1989@gmail.com>
2023-11-14 19:28:24 -0500

    352. Añadiendo los datos del Formulario al State y Validando

--- src/components/FormDrink.jsx
+++ src/components/FormDrink.jsx
@@ -1,10 +1,34 @@
-import { Button, Form, Col, Row } from "react-bootstrap";
+import { Button, Form, Col, Row, Alert } from "react-bootstrap";
 import useCategory from "../hooks/useCategory";
+import { useState } from "react";
 
 export const FormDrink = () => {
   const { categories } = useCategory();
+  const [alert , setAlert] = useState(false);
+  const [search, setSearch] = useState({
+    name: "",
+    category: "",
+  });
+
+  const handleSubmit = e => { 
+    e.preventDefault();
+
+    if(Object.values(search).includes('')){
+      setAlert('All Fields are Required.!');
+      return;
+    }
+
+    setAlert(false);
+
+    
+
+  }
+
   return (
-    <Form>
+    <Form
+    onSubmit={handleSubmit}
+    >
+      {alert && <Alert variant="danger" className="textr-center">{alert}</Alert> }
       <Row>
         <Col md={6}>
           <Form.Group className="mt-3">
@@ -14,13 +38,30 @@ export const FormDrink = () => {
               type="text"
               placeholder="Ex: Tequila, Vodka, Etc..."
               name="name"
+              value={search.name}
+              onChange={(e) =>
+                setSearch({
+                  ...search,
+                  [e.target.name]: e.target.value,
+                })
+              }
             />
           </Form.Group>
         </Col>
         <Col md={6}>
           <Form.Group className="mt-3">
             <Form.Label htmlFor="category">Drink Category</Form.Label>
-            <Form.Select id="category">
+            <Form.Select
+              id="category"
+              name="category"
+              value={search.category}
+              onChange={(e) =>
+                setSearch({
+                  ...search,
+                  [e.target.name]: e.target.value,
+                })
+              }
+            >
               <option>-Select a Category-</option>
               {categories?.map((cat) => (
                 <option key={cat.strCategory} value={cat.strCategory}>
@@ -30,11 +71,13 @@ export const FormDrink = () => {
             </Form.Select>
           </Form.Group>
         </Col>
-      <Row className="justify-content-end">
-        <Col md={3}>
-        <Button variant="danger" className="text-uppercase mt-3 w-100">Search Drinks</Button>
-        </Col>
-      </Row>
+        <Row className="justify-content-end">
+          <Col md={3}>
+            <Button type="submit" variant="danger" className="text-uppercase mt-3 w-100">
+              Search Drinks
+            </Button>
+          </Col>
+        </Row>
       </Row>
     </Form>
   );
